112cc6a67042b266bfca8780fe44b41c06622b9b
Andrius <gironandres.valencia@gmail.com>
2025-09-13 15:36:18 -0500

    New PJ

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+pequeño proyecto con python y mysql en consola
+- Abrir asistente
+- login o registro
+- Si elegimos registro creare un usuario en la base de datos
+- Si elegimos login verificare si el usuario existe
+- Crear nota, mostrar notas, borrarlas.
+
+"""
+
