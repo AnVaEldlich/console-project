879843e9517b66b172edec91ce1899ca707eb320
Andrius <gironandres.valencia@gmail.com>
2025-09-15 12:34:37 -0500

    changes

--- usuarios/usuario.py
+++ usuarios/usuario.py
@@ -1,2 +1,29 @@
+import mysql.connector
+
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="3080",
+    database="master_python",
+    port=3306
+)
+
+cursor = database.cursor(buffered=True)
+
+
 class Usuario:
+
+    def __init__(self, nombre, appelido, email, password):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.email = email
+        self.password = password
+
+
+    def registrar(self):
+        return self.nombre
     
+    def identificar(self):
+        return self.nombre
+
